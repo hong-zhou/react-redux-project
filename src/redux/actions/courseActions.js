04e1fa8112428a78d53810dbf74104e4e282fb6c
Hong Zhou <zhsnnu@gmail.com>
2019-11-08 23:25:15 -0600

    add redux-thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,4 +1,23 @@
 import * as types from "./actionTypes";
+import * as couseApi from "../../api/courseApi";
+
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSE_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return couseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,4 @@
- const webpack = require("webpack");
+const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -8,7 +8,7 @@ module.exports = {
   mode: "development",
   target: "web",
   devtool: "cheap-module-source-map",
-  entry: "./src/index", 
+  entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
@@ -23,6 +23,9 @@ module.exports = {
     https: false
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico"
