a7bd0741959f32d7200a3ba31621cf31f46dce7f
Hong Zhou <zhsnnu@gmail.com>
2020-06-29 22:57:13 -0500

    implemented optimistic deletes , async/await

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -5,3 +5,6 @@ export const CREATE_COURSE_SUCCESS = "CREATE_COURSE_SUCCESS";
 export const UPDATE_COURSE_SUCCESS = "UPDATE_COURSE_SUCCESS";
 export const BEGIN_API_CALL = "BEGIN_API_CALL";
 export const API_CALL_ERROR = "API_CALL_ERROR";
+// By convention, actions that end in "_SUCCESS" are assumed to have been the result of a completed API call.
+// apiCallStatus reducer looks for action types that end in "_SUCCESS"
+export const DELETE_COURSE_OPTIMISTIC = "DELETE_COURSE_OPTIMISTIC";
